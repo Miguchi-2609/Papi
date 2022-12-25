ecf2efb61335cdd9b32419f936851d9f3e3fafcf
gab swin <96878796+Miguchi-2609@users.noreply.github.com>
2022-12-25 17:50:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import time
 import threading
 import glob
 import os
-from update_check
-import isUpToDate
+from update_check import isUpToDate
 
 
 window = Tk()
@@ -26,7 +25,6 @@ button = Button(text="Retourner sur Windows 11", command=quit)
 button.pack(side=BOTTOM)
 
 
-from update_check import isUpToDate
 
 if isUpToDate(__file__, "https://github.com/Miguchi-2609/Papi/blob/dbde9afe8683ea5133a1c76b64dba31f8ca77622/main.py") == False:
    doSomething()
