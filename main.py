39a74eefd327f0cdd736f665653f2218d6abc7bb
gab swin <96878796+Miguchi-2609@users.noreply.github.com>
2022-12-25 17:48:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import time
 import threading
 import glob
 import os
-from update_check import isUpToDate
+from update_check
+import isUpToDate
 
 
 window = Tk()
@@ -27,7 +28,7 @@ button.pack(side=BOTTOM)
 
 from update_check import isUpToDate
 
-if isUpToDate(__file__, "https://raw.githubusercontent.com/username/repo/myProgram.py") == False:
+if isUpToDate(__file__, "https://github.com/Miguchi-2609/Papi/blob/dbde9afe8683ea5133a1c76b64dba31f8ca77622/main.py") == False:
    doSomething()
 
 
