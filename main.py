dbde9afe8683ea5133a1c76b64dba31f8ca77622
gab swin <96878796+Miguchi-2609@users.noreply.github.com>
2022-12-25 17:46:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+from logging import root
+from tkinter import *
+from tkinter import messagebox
+from tkinter.tix import LabelEntry
+import webbrowser
+import time
+import threading
+import glob
+import os
+from update_check import isUpToDate
 
+
+window = Tk()
+window.title("Papy")
+window.iconbitmap(r'C:\Users\mitem\Desktop\code\logotest.ico')
+label = Label(window, text="Bonjour papy!", font = ( "Arial Bold" , 30 ), fg ="white", bg = "black")
+label.pack(side=LEFT)
+label = Label(window, text="comment va", font = ( "Arial Bold" , 30 ), fg ="white", bg = "black")
+label.pack()
+
+
+
+
+button = Button(text="Retourner sur Windows 11", command=quit)
+button.pack(side=BOTTOM)
+
+
+from update_check import isUpToDate
+
+if isUpToDate(__file__, "https://raw.githubusercontent.com/username/repo/myProgram.py") == False:
+   doSomething()
+
+
+
+ 
+window.attributes("-fullscreen", True)
+window.configure(bg='black')
+window.mainloop()
+exit(0)
